3e2729146d4da2665a3f7fb0aafc83345d0aa5d1
unknown <dudu94bhz@gmail.com>
2023-07-13 18:46:08 -0300

    Add menu mobile

--- scripts/script.js
+++ scripts/script.js
@@ -120,4 +120,18 @@ function changeBodyBgImage() {
   }
   
   
-  
+
+  // Menu responsivo
+const icon = document.querySelector('#burguer')
+const menu = document.querySelector('#menu-mobile')
+
+icon.addEventListener ('click', clickMenu )
+
+function clickMenu() {
+    if (menu.style.display == 'block'){
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
+
